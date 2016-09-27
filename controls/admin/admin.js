245582cbaaf6ea6476b53c0fd5c1d13ef4fb1398
clown-doll <990515207@qq.com>
2016-09-27 18:02:06 +0800

    修改日期过期判断方式及前台展现形式

--- controls/admin/admin.js
+++ controls/admin/admin.js
@@ -10,15 +10,30 @@ var moment = require('moment');
 
 module.exports = {
     home: function *(next) {
-        var courses = yield models.Course.find({});
         try {
+            var courses = yield models.Course.find({});
+            var finished = [],
+                unfinished = [];
+
+            for (var i = 0; i < courses.length; i++) {
+                var flag = moment(courses[i].time).isBefore(new Date()); // 判断课程是否在当前时间之前
+                console.log(courses[i].time);
+                if (flag) {
+                    finished.push(courses[i]);
+                } else {
+                    unfinished.push(courses[i]);
+                }
+            }
+
             yield this.render('admin/index', {
                 layout: 'admin/layout',
                 title: "课程管理",
-                moment: moment,
-                courses: courses,
-                username: this.session.username
+                finished: finished,
+                unfinished: unfinished,
+                username: this.session.username,
+                moment: moment
             });
+
         } catch (e) {
             this.body = "发生错误";
             console.log(e);
--- controls/app/index.js
+++ controls/app/index.js
@@ -12,18 +12,38 @@ module.exports = {
     home: function *(next) {
         console.log('index');
         try {
-            var courses = yield models.Course.find({}).sort({time: -1});
-            console.log(this.session);
+            var courses = yield models.Course.find({}).sort({time: 1});
+            var finished = [],
+                unfinished = [];
+
+            // 课程分类
+            for (var i = 0; i < courses.length; i++) {
+                var flag = moment(courses[i].time).isBefore(new Date()); // 判断课程是否在当前时间之前
+                console.log(courses[i].time);
+                if (flag) {
+                    finished.push(courses[i]);
+                } else {
+                    unfinished.push(courses[i]);
+                }
+            }
+
+            //console.log(courses);
+            //console.log(finished);
+            //console.log(unfinished);
+
             if (this.session.username) {
                 var username = this.session.username;
             }
+
             yield this.render('app/index', {
                 layout: 'app/layout',
                 title: "团队培训",
                 username: username,
-                moment: moment,
-                courses: courses
+                finished: finished,
+                unfinished: unfinished,
+                moment: moment
             });
+
         } catch (e) {
             this.body = '加载错误';
             console.log(e);
